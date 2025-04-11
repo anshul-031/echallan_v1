4ddee84ea9f904e2bcac18264fb2f2a83f46bae9
Karanrajsinh <vaghelakaranraj54@gmail.com>
2025-04-11 23:13:35 +0530

    admin ,dashboard ,profile

--- app/api/profile/update/route.ts
+++ app/api/profile/update/route.ts
@@ -21,7 +21,7 @@ export async function PUT(req: Request) {
     const session = await getServerSession(authOptions);
     if (!session?.user?.email) {
       return new NextResponse("Unauthorized", { status: 401 });
-    }
+    } 
 
     const data: ProfileUpdateInput = await req.json();
     const validUpdateData: Prisma.UserUpdateInput = {};
